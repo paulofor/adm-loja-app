634c392670e330ed3fd4ecf7e515cfb95d5d6687
Paulo Alexandre <paulofore@gmail.com>
2018-08-25 16:53:37 -0300

    nao sei se mudei

--- src/app/adm-routing/adm-routing.module.ts
+++ src/app/adm-routing/adm-routing.module.ts
@@ -5,18 +5,19 @@ import { ProdutoComponent } from '../produto/produto.component';
 import { PedidoComponent } from '../pedido/pedido.component';
 import { AdmHomeComponent } from '../adm-home/adm-home.component';
 
-
+/*
 const admRotas: Routes = [
   { path : 'produto' , component: ProdutoComponent  },
   { path : 'pedido' , component: PedidoComponent},
   { path : 'home' , component: AdmHomeComponent}
 ]
+*/
 
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(admRotas)
+    //RouterModule.forChild(admRotas)
   ],
   declarations: []
 })
--- src/app/prinicipal-routing/prinicipal-routing.module.ts
+++ src/app/prinicipal-routing/prinicipal-routing.module.ts
@@ -3,12 +3,25 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { PrincipalComponent } from '../principal/principal.component';
 import { LoginComponent } from '../login/login.component';
+import { ProdutoComponent } from '../produto/produto.component';
+import { PedidoComponent } from '../pedido/pedido.component';
+import { AdmHomeComponent } from '../adm-home/adm-home.component';
 
 
 const principalRotas: Routes = [
   { path : '' , component: LoginComponent  },
-  { path : 'home' , component: PrincipalComponent}
+  { 
+    path : 'home' , component: PrincipalComponent,
+    children : [
+      { path : 'produto' , component: ProdutoComponent  },
+      { path : 'pedido' , component: PedidoComponent},
+      { path : 'home' , component: AdmHomeComponent}
+    ]
+  }
 ]
+
+
+
 
 @NgModule({
   imports: [
